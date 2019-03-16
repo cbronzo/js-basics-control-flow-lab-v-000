9beb7978272334820b918f6d47aa8d064e95eeba
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 03:39:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Write your code in this file!
 
 functon scuberGreetingForFree(someValue) {
-  if (someValue <= 400) {'This one is on me!'
+  if (someValue <= 400) {
+    'This one is on me!'
   }
-  else if (someValue > 400) {''
-  
-}
-else (someValue > 2500){'No can do.'
-}
+  else if (someValue > 2000) {
+    'I will gladly take your thirty bucks.'
+  }
+  else (someValue > 2500){
+    'No can do.'
+  }
 
-This one is on me!

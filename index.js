31d2bd965c68c8d04fb46966f1fe789728827c69
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 03:43:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Write your code in this file!
 
 
-function scuberGreetingForFree(someValue) {
+function scuberGreetingForFree(someNumber) {
   let result;
-  if (someValue <= 400) {
+  if (someNumber <= 400) {
     result = 'This one is on me!';
   }
-  else if (someValue > 2000) {
+  else if (someNumber > 2000) {
     result = 'I will gladly take your thirty bucks.';
   }
-  else (someValue > 2500){
+  else (someNumber > 2500){
    result = 'No can do.';
   }
   return result;

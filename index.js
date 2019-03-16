c245813155f640c8ca76d986e3222e96d589951c
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 03:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function scuberGreetingForFree(someNumber) {
   if (someNumber > 2000) {
     result = 'I will gladly take your thirty bucks.';
   }
-  else (someNumber > 2500){
+  if (someNumber > 2500){
    result = 'No can do.';
   }
   return result

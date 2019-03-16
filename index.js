0aa8b20088f64a7029f9f328f12f7a9133bc5d79
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 03:48:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function scuberGreetingForFeet(someNumber) {
 
 
 function ternaryCheckCity(city) {
-  return (city === NYC) ?  "Ok, sounds good." : "No go.");
+  return (city === 'NYC') ? "Ok, sounds good." : "No go.");
 }
+
 
 

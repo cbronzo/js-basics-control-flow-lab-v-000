8891753f7e26b886d54e850b234380e386330094
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 03:44:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,3 @@ function scuberGreetingForFree(someNumber) {
 
 
 
-	if (someNumber > 2000) {
-		result = "I will gladly take your thirty bucks.";
-	}
-	if (someNumber > 2500) {
-		result = "No can do.";
-	}
-	return result
-}

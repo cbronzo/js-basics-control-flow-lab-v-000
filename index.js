f11f87cbead642f81157d958ddfda7af7a0a59f1
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 03:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,8 @@ function ternaryCheckCity(city) {
   return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.');
 }
 
+function ternaryCheckCity(city) {
+	return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+}
 
 
